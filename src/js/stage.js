f4eafb5ea72050c225ab0565054ed9126f17ec6f
Jake 'Sid' Smith <jake@dabapps.com>
2018-04-04 11:36:53 +0100

    Half canvas density

--- src/js/stage.js
+++ src/js/stage.js
@@ -10,7 +10,7 @@
   window.Stage = function Stage (element) {
     var self = this;
     var shapes = [];
-    var canvas = new Canvasimo(element);
+    var canvas = new Canvasimo(element).setDensity(0.5);
 
     function rotateDirection (direction, axis1, axis2, v, r) {
       var distance = canvas.getDistance(0, 0, v[axis1], v[axis2]);
